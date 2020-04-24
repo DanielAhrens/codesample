927a7ade9761ae46cc64419c317053d0df6292c3
Daniel Ahrens <ahrens.d@gmail.com>
2020-04-24 17:13:41 -0500

    Realized I could do the subtotals more cleanly.

--- codesample/src/main/java/com/dahrens/codesample/Sample.java
+++ codesample/src/main/java/com/dahrens/codesample/Sample.java
@@ -5,15 +5,14 @@ import java.util.List;
 public class Sample {
     /** Given two lists of any lengths, return a single integer that is the sum of the values in both lists **/
    public static Integer sumLists(List<Integer> list1, List<Integer> list2) {
-        Integer subtotal1 = 0;
-        Integer subtotal2 = 0;
+        Integer total = 0;
         if (list1 != null) {
-            subtotal1 = list1.stream().reduce(0, Integer::sum);
+            total = list1.stream().reduce(total, Integer::sum);
         }
         if (list2 != null) {
-            subtotal2 = list2.stream().reduce(0, Integer::sum);
+            total = list2.stream().reduce(total, Integer::sum);
         }
-        return subtotal1 + subtotal2;
+        return total;
     }
 
 
